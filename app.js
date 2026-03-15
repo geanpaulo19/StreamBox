e397aacd1c337f2a1ce0e35a5200ca84aa99b4de
Gean Paulo <geanpaulofrancois@gmail.com>
2026-03-15 13:26:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1069,7 +1069,7 @@ function showResumeOverlay() {
   overlay.id = 'resume-overlay';
   overlay.innerHTML = `
     <button id="resume-btn">
-      <svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor"><polygon points="5 3 19 12 5 21 5 3"/></svg>
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><polygon points="5 3 19 12 5 21 5 3"/></svg>
       Resume
     </button>
   `;
